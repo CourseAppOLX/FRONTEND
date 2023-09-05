005ac646645361fb05e38f667b89381d49396f0f
Pavlo Brychka <brychkapavlo.work@gmail.com>
2023-09-05 08:13:31 +0300

    loginpage

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import LoginPage from './components/auth/login/LoginPage';
+import LoginPage from './components/auth/login/LoginPage';
+import { Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <Routes>
+    <Route path="login" element={LoginPage}/>
+  </Routes>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
